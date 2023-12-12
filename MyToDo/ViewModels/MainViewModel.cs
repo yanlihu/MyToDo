430722c1da654dbfec67bcd9b3accdadf513c965
yanlihu <yanlihu@DESKTOP-UAHVFUP>
2023-12-12 17:07:29 +0800

    添加项目文件。

---
+++ MyToDo/ViewModels/MainViewModel.cs
@@ -0,0 +1,52 @@
+﻿using MyToDo.Common.Models;
+using MyToDo.Extensions;
+using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Regions;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyToDo.ViewModels
+{
+    class MainViewModel:BindableBase
+    {
+        public MainViewModel(IRegionManager regionManager)
+        {
+            MenuBars=new ObservableCollection<MenuBar>();
+            CreateMenuBars();
+            NavigateCommand = new DelegateCommand<MenuBar>(Navigate);
+            this.regionManager = regionManager;
+        }
+
+        private void Navigate(MenuBar menubar)
+        {
+            if (menubar == null || string.IsNullOrWhiteSpace(menubar.NameSpace))
+            {
+                return;
+            }
+            regionManager.Regions[PrismManager.MainViewRegionName].RequestNavigate(menubar.NameSpace);
+        }
+
+        public DelegateCommand<MenuBar> NavigateCommand { get; set; } 
+
+        private ObservableCollection<MenuBar> menuBars;
+        private readonly IRegionManager regionManager;
+
+        public ObservableCollection<MenuBar> MenuBars
+        {
+			get { return menuBars; }
+			set { menuBars = value;RaisePropertyChanged(); }
+		}
+		void CreateMenuBars()
+		{
+			MenuBars.Add(new MenuBar() { Icon = "Home", Title = "首页", NameSpace = "IndexView" });
+            MenuBars.Add(new MenuBar() { Icon = "NotebookCheckOutline", Title = "待办事项", NameSpace = "ToDoView" });
+            MenuBars.Add(new MenuBar() { Icon = "NotebookHeartOutline", Title = "备忘录", NameSpace = "MemoView" });
+            MenuBars.Add(new MenuBar() { Icon = "Cog", Title = "设置", NameSpace = "SettingsView" });
+        }
+	}
+}
---
+++ MyToDo/Views/MainView.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MyToDo.Views
+{
+    /// <summary>
+    /// MainView.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainView : Window
+    {
+        public MainView()
+        {
+            InitializeComponent();
+            btnMin.Click += (s, e) => { this.WindowState = WindowState.Minimized; };
+            btnMax.Click += (s, e) => {
+                if (this.WindowState == WindowState.Maximized)
+                { 
+                    this.WindowState = WindowState.Normal;
+                    return;
+                }
+                this.WindowState = WindowState.Maximized;
+            };
+            btnClose.Click += (s, e) => { this.Close(); };
+            ColorZone.MouseMove += (s, e) => {
+                if (e.LeftButton == MouseButtonState.Pressed)
+                {
+                    this.DragMove();
+                }
+            };
+        }
+    }
+}
